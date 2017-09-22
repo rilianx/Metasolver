8871c00e2e68ca988474428673233836a15bfbc3
iaraya <ignacio.araya@pucv.cl>
2017-09-22 09:49:15 -0300

    add MO_ActionEvaluator and MO_clpBasicEvaluator

--- metasolver/ActionEvaluator.h
+++ metasolver/ActionEvaluator.h
@@ -36,6 +36,11 @@ public:
 
 	virtual double eval_action(const State& s, const Action& a) =0;
 
+	double get_r() const { return r; }
+
+protected:
+
+
 	private:
 	double r;
 
@@ -43,17 +48,36 @@ public:
 
 class MO_ActionEvaluator : public ActionEvaluator{
 public:
-	MO_ActionEvaluator(double r) : ActionEvaluator (r), alpha (1.0) { }
+	MO_ActionEvaluator(ActionEvaluator* ev1=NULL, ActionEvaluator* ev2=NULL, double r=0.0) :
+		ev1(ev1), ev2(ev2), ActionEvaluator(r), alpha(1.0) { }
 
 	virtual ~MO_ActionEvaluator(){ }
+
+	virtual double eval_action(const State& s, const Action& a){
+		double ev=eval_action1(s,a);
+		double evv=eval_action2(s,a);
+
+		if(ev1) ev = ev1->eval_action(s, a);
+		if(ev2) evv = ev2->eval_action(s, a);
+
+		return alpha*ev + (1.0-alpha)*evv;
+	}
 
 	//set the value of alpha
 	virtual void set_alpha(double a){
 		alpha = a;
 	}
 
+
 protected:
+	//for the first and second objectives
+	virtual double eval_action1(const State& s, const Action& a) {return 0.0;}
+	virtual double eval_action2(const State& s, const Action& a) {return 0.0;}
+
+private:
 	double alpha;
+	ActionEvaluator* ev1;
+	ActionEvaluator* ev2;
 
 };
 
