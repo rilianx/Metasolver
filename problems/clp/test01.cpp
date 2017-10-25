ddd862e7aea139250be6597b2b05db91b08a0ed4
iaraya <ignacio.araya@pucv.cl>
2017-10-25 16:53:59 -0300

    Added catch framework for tests

---
+++ problems/clp/test01.cpp
@@ -0,0 +1,24 @@
+/*
+ * test01.cpp
+ *
+ *  Created on: 25 oct. 2017
+ *      Author: iaraya
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+
+TEST_CASE("Hola mundo"){
+
+	REQUIRE(1==1);
+
+}
+
+
+TEST_CASE("Hola mundo2"){
+
+	REQUIRE(2==2);
+
+}
