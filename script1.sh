85aedff9b465008b1e2d6df3c6c006a3371c4fb2
Arielo1994 <mono_sensei@hotmail.com>
2017-12-11 11:32:50 -0300

    generacion de archivos de entrenamiento, solo bloques

--- script1.sh
+++ script1.sh
@@ -4,81 +4,81 @@ echo "Hola Mundo"
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data01.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data01.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data03.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data03.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data05.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data05.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data08.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data08.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data10.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data10.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data12.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data12.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data15.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data15.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data20.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data20.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data30.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data30.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data40.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data40.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data50.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data50.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data60.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data60.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data70.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data70.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data80.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data80.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data90.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data90.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
 
 for i in {1..99}
 do
-./BSG_CLP_ANN problems/clp/benchs/ann_a/data100.txt $i 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
+./BSG_CLP_ANN problems/clp/benchs/ann_a/data100.txt $i 0.98 2 4.0 1.0 0.2 0.04 1.0 0.0 0.0 1 0
 done
