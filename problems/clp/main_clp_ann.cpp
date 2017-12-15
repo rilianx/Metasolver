af6b899cd282e647d3971ac5ca46fd1993140dfa
Arielo1994 <mono_sensei@hotmail.com>
2017-12-15 17:09:14 -0300

    Se agrega cabecera a las instancias de entrenamiento generadas, indicando dimensiones del contenedor, numero de tipos de cajas, los tipos de cajas sus dimensiones cantidad y rotaciones permitidas.
    Se modifica el que recorre todas las instancias, de manera que resuelva
    en paralelo 4 instancias de clp al mismo tiempo.

--- problems/clp/main_clp_ann.cpp
+++ problems/clp/main_clp_ann.cpp
@@ -30,11 +30,13 @@ using namespace std;
 // para ejecutar (menos de 30 tipos de caja): BSG_CLP problems/clp/benchs/BR/BR7.txt 1 1.0 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 0 0
 // para ejecutar (mas de 30 tipos de caja): BSG_CLP problems/clp/benchs/BR/BR8.txt 1 0.98 30 4.0 1.0 0.2 0.04 1.0 0.0 0.0 0 0
 
+int n_tipos_cajas(string file);
 void whiteBackground( vector< vector< int > >& , int ) ;
 void printBlock(vector< vector< int > >& , vector<int> , vector<int> ,int );
 void printPgm(string ,vector< vector< int > > ,int  );
-
+string outputPath(string);
 void extract_boxes(AABB aabb, list< AABB >& uni_blocks);
+void addHeader(string, int , int,ofstream&);
 
 /**
  * comparacion para ordenar de menor a mayor altura
@@ -112,8 +114,137 @@ int main(int argc, char** argv){
 
 	cout << "running" << endl;
     double eval = 1-de->run(s_copy, max_time, begin_time) ;
-	cout << eval << endl;
-	 string path1="problems/clp/benchs/ann_a/"; //directorio de entrada;
+	cout << "eval"<<1-eval << endl;
+
+
+	string path=outputPath(file);
+
+//hasta ahora el path de output seria ej: "problems/clp/train_samples/10/"
+
+		cout<<"path:"<<path<<endl;
+
+
+		stringstream ss;
+		ss << inst;
+		string indiceInstancia = ss.str();
+
+		cout <<"indice instancia"<<indiceInstancia<<endl;
+
+		path=path+"trainSample";
+
+		cout<<"path:"<<path<<endl;
+
+	list<const Action*>& actions= dynamic_cast<const clpState*>(de->get_best_state())->get_path();
+
+	clpState* s00 = dynamic_cast<clpState*> (s0->clone());
+
+	const AABB* aux= &s00->cont->blocks->top();
+	int width=587,length=233, maxval=220;
+
+	ofstream myfile;
+	ofstream myfile1;
+
+	string path2;
+	path2=path+"Box"+indiceInstancia+".txt";
+	path=path+"Block"+indiceInstancia+".txt";
+	myfile.open(path);
+	myfile1.open(path2);
+
+	myfile << eval <<endl;
+
+	myfile1 << eval <<endl;
+
+	int n_tipos = n_tipos_cajas(file);
+
+	addHeader( file,  inst, n_tipos, myfile);
+	addHeader( file,  inst, n_tipos, myfile1);
+
+	cout<<"path:"<<path<<endl;
+
+
+	for(auto action:actions){
+
+		const clpAction* clp_action = dynamic_cast<const clpAction*> (action);
+
+		const AABB* aux= &s00->cont->blocks->top();
+
+		s00->transition(*clp_action);
+
+
+		cout << "block :" << clp_action->block << endl;
+
+		list< AABB > uni_blocks;
+		AABB aabb(clp_action->space.get_location(clp_action->block),&clp_action->block);
+		extract_boxes(aabb, uni_blocks);
+
+		uni_blocks.sort(compare_aabb);
+
+		for(auto aabb:uni_blocks){
+			//cout << aabb << endl;
+			myfile1 << aabb <<endl;
+		}
+
+
+		myfile << clp_action->block << " ; ";
+		//cout << "location :" << clp_action->space.get_location(clp_action->block) << endl;
+		myfile << clp_action->space.get_location(clp_action->block) << endl;
+
+	}
+	myfile.close();
+	myfile1.close();
+
+	//s00->cont->MatLab_print();
+
+
+}
+
+void addHeader(string input_path, int inst,int tipos, ofstream& output){
+	string line;
+	  ifstream myfile (input_path);
+
+	  int limsup, liminf;
+
+	  liminf=1+(inst)*(tipos+3);
+	  limsup=liminf+tipos+3;
+
+	  cout << "tipos:" <<tipos<<endl;
+	  cout <<"lim inf:"<<liminf<<endl;
+	  cout <<"lim sup:"<<limsup<<endl;
+	  if (myfile.is_open())
+	  {
+		    for (int lineno = 0; getline (myfile,line) && lineno < limsup; lineno++)
+		      if (lineno >= liminf)
+		    	  output << line << endl;
+
+	  }
+
+	  else cout << "Unable to open file";
+
+}
+
+int n_tipos_cajas(string file){
+	if(file.compare("problems/clp/benchs/ann_a/data01.txt")==0) return 1;//direccion de salida particular
+	if(file.compare("problems/clp/benchs/ann_a/data03.txt")==0) return 3;
+	if(file.compare("problems/clp/benchs/ann_a/data05.txt")==0) return 5;
+	if(file.compare("problems/clp/benchs/ann_a/data08.txt")==0) return 8;
+	if(file.compare("problems/clp/benchs/ann_a/data10.txt")==0) return 10;
+	if(file.compare("problems/clp/benchs/ann_a/data12.txt")==0) return 12;
+	if(file.compare("problems/clp/benchs/ann_a/data15.txt")==0) return 15;
+	if(file.compare("problems/clp/benchs/ann_a/data20.txt")==0) return 20;
+	if(file.compare("problems/clp/benchs/ann_a/data30.txt")==0) return 30;
+	if(file.compare("problems/clp/benchs/ann_a/data40.txt")==0) return 40;
+	if(file.compare("problems/clp/benchs/ann_a/data50.txt")==0) return 50;
+	if(file.compare("problems/clp/benchs/ann_a/data60.txt")==0) return 60;
+	if(file.compare("problems/clp/benchs/ann_a/data70.txt")==0) return 70;
+	if(file.compare("problems/clp/benchs/ann_a/data80.txt")==0) return 80;
+	if(file.compare("problems/clp/benchs/ann_a/data90.txt")==0) return 90;
+	if(file.compare("problems/clp/benchs/ann_a/data100.txt")==0) return 100;
+
+	return 0;
+
+}
+string outputPath(string file){
+	string path1="problems/clp/benchs/ann_a/"; //directorio de entrada;
 	 string path="problems/clp/train_samples/"; //directorio de salida;
 		if(file.compare(path1+"data01.txt")==0)path=path+"1/";//direccion de salida particular
 		if(file.compare(path1+"data03.txt")==0)path=path+"3/";
@@ -132,144 +263,14 @@ int main(int argc, char** argv){
 		if(file.compare(path1+"data90.txt")==0)path=path+"90/";
 		if(file.compare(path1+"data100.txt")==0)path=path+"100/";
 
-
-//hasta ahora el path de output seria ej: "problems/clp/train_samples/10/"
-
-
-
-
-		cout<<"path:"<<path<<endl;
-
-
-		stringstream ss;
-		ss << inst;
-		string indiceInstancia = ss.str();
-
-		cout <<"indice instancia"<<indiceInstancia<<endl;
-
-		path=path+"trainSample";
-
-		cout<<"path:"<<path<<endl;
-
-	list<const Action*>& actions= dynamic_cast<const clpState*>(de->get_best_state())->get_path();
-  //  actions.sort(clpState::height_sort);
-
-
-	clpState* s00 = dynamic_cast<clpState*> (s0->clone());
-
-
-
-	const AABB* aux= &s00->cont->blocks->top();
-	int width=587,length=233, maxval=220;
-
-	//int image[width][length];
-
-	 //vector<vector<int> > imageMatrix(width, vector<int>(length, 0));
-
-	//whiteBackground(imageMatrix, maxval);
-	ofstream myfile;
-	path=path+indiceInstancia+".txt";
-	myfile.open(path);
-
-
-	cout<<"path:"<<path<<endl;
-
-
-
-
-	//ofstream img ("picture.pgm");
-	//recorro cada accion
-	string sampleName=path;
-	int i=0;
-
-/*	const clpState* sol = dynamic_cast<const clpState*>(de->get_best_state());
-
-	list< AABB > uni_blocks;
-	extract_boxes(AABB( Vector3(0,0,0), sol->cont), uni_blocks);
-
-	uni_blocks.sort(compare_aabb);
-
-	cout << uni_blocks.size() << endl;
-	for(auto aabb:uni_blocks){
-		cout << aabb << endl;
-	}
-
-	return 1;*/
-
-	for(auto action:actions){
-
-		const clpAction* clp_action = dynamic_cast<const clpAction*> (action);
-		//ofstream img ("picture"+i+".pgm");
-		//recorro los bloques
-		const AABB* aux= &s00->cont->blocks->top();
-		cout<<"recorro los bloques"<<endl;
-	    while(true){
-			/*//...
-	    	//aqui deberia formar
-			if(!(s00->is_root())) {
-			cout<<"bloque:"<<endl;
-	    	cout<<"xmax:"<<aux->getXmax()<<endl;
-	    	cout<<"ymax:"<<aux->getYmax()<<endl;
-	    	cout<<"zmax:"<<aux->getZmax()<<endl;
-	    	cout<<"xmin:"<<aux->getXmin()<<endl;
-	    	cout<<"ymin:"<<aux->getYmin()<<endl;
-	    	cout<<"zmin:"<<aux->getZmin()<<endl;
-
-			}else{
-				cout<<"no hay bloques"<<endl;
-				//string filename=
-				//printPgm();
-			}*/
-	    	if(s00->cont->blocks->has_next())
-			  aux=&s00->cont->blocks->next();
-
-	    	else break;
-		}
-	    //pongo un bloque
-		s00->transition(*clp_action);
-		//s00->nb_left_boxes;
-		//cout<<"pongo un bloque"<<endl;
-		//imprimo el bloque puesto y su posicion
-
-		cout << "block :" << clp_action->block << endl;
-
-		/*
-		list< AABB > uni_blocks;
-		extract_boxes(AABB( clp_action->space.get_location(clp_action->block), clp_action->block), uni_blocks);
-
-		uni_blocks.sort(compare_aabb);
-
-		cout << uni_blocks.size() << endl;
-		for(auto aabb:uni_blocks){
-			cout << aabb << endl;
-		}*/
-
-
-		myfile << clp_action->block << endl;
-		cout << "location :" << clp_action->space.get_location(clp_action->block) << endl;
-		myfile << clp_action->space.get_location(clp_action->block) << endl;
-
-		i++;
-	}
-	myfile.close();
-
-/*
-	const AABB* b = &dynamic_cast<const clpState*>(de->get_best_state())->cont->blocks->top();
-	while(dynamic_cast<const clpState*>(de->get_best_state())->cont->blocks->has_next()){
-		cout << *b << ":" << b->getVolume() << "(" << b->getOccupiedVolume() << ")" << endl;
-		b = &dynamic_cast<const clpState*>(de->get_best_state())->cont->blocks->next();
-	}
-*/
-
-
-	s00->cont->MatLab_print();
-
-
+	return path;
 }
 
-
 void extract_boxes(AABB aabb, list< AABB >& uni_blocks){
+
 	const Block* block=aabb.getBlock();
+	//cout << block << endl;
+
 	Vector3 pos = aabb.getMins();
 
 	if(block->n_boxes==1){
@@ -278,7 +279,6 @@ void extract_boxes(AABB aabb, list< AABB >& uni_blocks){
 	}
 
 	const AABB* aux= &block->blocks->top();
-
 	while(true){
 
 		AABB aabb2 (pos+aux->getMins(), aux->getBlock());
@@ -289,6 +289,7 @@ void extract_boxes(AABB aabb, list< AABB >& uni_blocks){
 
 		else break;
 	}
+
 }
 
 
