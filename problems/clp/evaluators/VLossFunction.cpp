8ff5eea85b465617b7391a2cd5fba7aa98405cde
iaraya <ignacio.araya@pucv.cl>
2018-04-21 22:40:23 -0300

    testing

--- problems/clp/evaluators/VLossFunction.cpp
+++ problems/clp/evaluators/VLossFunction.cpp
@@ -74,7 +74,7 @@ double VLossFunction::eval_action(const State& s,  const Action &a){
 
 	//cout << theta << endl;
 
-	return ( pow(vol,delta)  * pow((1.0-loss),beta) * pow(density, theta ));
+	return ( pow( vol , delta)  * pow((1.0-loss),beta) * pow(density, theta ));
 
 //	return ( delta * log(vol) + beta * log(1.0-loss) + theta * log(b.getTotalWeight()/clpState::weight_of_allboxes) );
 }
